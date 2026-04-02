12e25b6cf3c9af9a91442e02587408e1b37bc35f
thatzcool <mycaptain0771@gmail.com>
2026-04-02 16:40:50 +0900

    rest-api_complete

--- 6_1Vuejs/vuejs_ex/vuejs07/rest_api_1/src/App.vue
+++ 6_1Vuejs/vuejs_ex/vuejs07/rest_api_1/src/App.vue
@@ -6,18 +6,23 @@ const getData = () => {
     .then((response) => {
       console.log(response.data);
     })
-    .catch(() => {
-      console.log(`데이터를 가져오던 중 에러 발생:`, error);
+    .catch((error) => {
+      console.error(`데이터를 가져오던 중 에러 발생:`, error);
     });
 };
 
-const postData = () => {
-  axios
-    .post('/api/data', { name: `new Data`, description: `sample data` })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch(console.log(`데이터를 추가하던 중 에러 발생:`, error));
+const postData = async () => {
+  try {
+    const response = await axios.post('/api/data', {
+      name: '국민이',
+      description: '테스트 데이터',
+    });
+    console.log('등록성공', response.data);
+    alert('등록이 완료되엇습니다.');
+  } catch (error) {
+    console.error('등록 실패', error);
+    alert('등록 중 문제가 발생했습니다.');
+  }
 };
 </script>
 
