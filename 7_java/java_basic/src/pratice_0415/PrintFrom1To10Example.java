fc379a035cab5717e4a8d3cb1c82db8172ac4ca7
thatzcool <mycaptain0771@gmail.com>
2026-04-15 15:48:14 +0900

    day02/homework_printFrom1To10Example

--- 7_java/java_basic/src/pratice_0415/PrintFrom1To10Example.java
+++ 7_java/java_basic/src/pratice_0415/PrintFrom1To10Example.java
@@ -6,6 +6,8 @@ package pratice_0415;
 * */
 public class PrintFrom1To10Example {
     public static void main(String[] args) {
-
+       for(int i = 1; i <=10 ; i++){
+           System.out.print(i + " ");
+       }
     }
 }
