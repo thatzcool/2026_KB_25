f522d4a5b1f1236ec1f68bb57a7ee8981a07163d
thatzcool <mycaptain0771@gmail.com>
2026-04-15 15:37:17 +0900

    day02/homework_IfElseELleseExample

--- 7_java/java_basic/src/pratice_0415/IfElseIfElseExample.java
+++ 7_java/java_basic/src/pratice_0415/IfElseIfElseExample.java
@@ -8,13 +8,37 @@ package pratice_0415;
     90~100 A등급
 
   출력:
-   점수가70~79입니다.
+   점수가 70~79입니다.
    등급은C입니다.
 
  */
 
 public class IfElseIfElseExample {
     public static void main(String[] args) {
+
         int score = 75;
+        if(score >= 90 && score <=100){
+            System.out.println("점수가 100~90점 사이입니다.");
+            System.out.println("등급은 A 입니다.");
+        }
+        else if(score >= 80 && score <=89){
+            System.out.println("점수가 80~89점 사이입니다.");
+            System.out.println("등급은 B 입니다.");
+        }
+        else if(score >= 70 && score <=79){
+            System.out.println("점수가 70~79점 사이입니다.");
+            System.out.println("등급은 C 입니다.");
+        }
+        else if(score >= 0  && score <=69){
+            System.out.println("점수가 0~69점 사이입니다.");
+            System.out.println("등급은 D 입니다.");
+        } else {
+            System.out.println("유효하지 않은 숫자 입니다.");
+        }
+
+
+
+
+
     }
 }
