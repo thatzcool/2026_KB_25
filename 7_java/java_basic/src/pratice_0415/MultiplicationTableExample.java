81789ee49b97f135a83d2f444ba9208d68219624
thatzcool <mycaptain0771@gmail.com>
2026-04-15 16:17:27 +0900

    day02/homework_MultiplicationTableExample

--- 7_java/java_basic/src/pratice_0415/MultiplicationTableExample.java
+++ 7_java/java_basic/src/pratice_0415/MultiplicationTableExample.java
@@ -5,11 +5,10 @@ package pratice_0415;
 * 출력조건
 *
   *** 2단 ***
-  2 x 1 = 2
-  2 x 2 = 4
-  2 x 3 = 6
-      …
-  *** 9단 ***
+  2 x 1 = 2    2 x 2 = 4   2 x 3 = 6       2 x 9 =  18      …
+  3 x 1 = 3    3 x 2 = 6   3 x 3 = 9   ----3 x 9 =  27
+
+  * *** 9단 ***
   9 x 1 = 9
   9 x 2 = 18
       …
@@ -19,4 +18,14 @@ package pratice_0415;
 * */
 
 public class MultiplicationTableExample {
+    public static void main(String[] args) {
+         for(int dan = 2 ; dan <= 9 ; dan++){
+             System.out.println("*** " + dan +"단 ***");
+             for (int column = 1; column <= 9; column++) {
+                 System.out.println( dan + " x " + column + " = " + dan * column + "    ");
+             }
+
+         }
+    }
+
 }
