5cacf93892fcbb9aca818975fd6f5474f69e20cf
thatzcool <mycaptain0771@gmail.com>
2026-04-16 14:32:23 +0900

    day03/printf

--- 7_java/java_basic/src/day03/FourArithmetic_method.java
+++ 7_java/java_basic/src/day03/FourArithmetic_method.java
@@ -5,16 +5,43 @@ public class FourArithmetic_method {
         int a, b ,result;   //변수 선언
         a = 12;   //값 할당
         b = 2;
+        String str1 = "Hello , Python";
+        float f1 = 3.1254f;
 
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
-        fourMethod(a,b);
+        System.out.printf("안녕 printf()\n");
+        System.out.printf("인사말 : %s\n",str1);      //문자열 %s
+        System.out.printf("a = %d , b = %d\n", a,b);   //정수 %d
+        System.out.printf("f1 = %f\n",f1);    //실수
+        System.out.printf("f1 = %.2f\n",f1);  // 실수 소숫점 처리
+        System.out.printf("번호: %5d\n",10);  //우측정렬
+        System.out.printf("번호: %-5d\n",10);  //좌측정렬
+        System.out.printf("번호: %05d\n",10);  //우측정렬
+
+        String item = "노트북";
+        int quantity = 10;
+        double price = 1500000.48;
+
+        System.out.printf("상품명 : %s , 현재 남은 수량 : %d , 한대당 가격 : %.1f원 , 총금액 : %.1f 달러\n",item, quantity,price,quantity*price);
+
+        double rate = 87.5;
+        System.out.printf("성공률 : %.1f%%\n",rate);
+
+        String userId = "Admin";
+        String action = "service debugger";
+        System.out.printf("[INFO] 사용자 : %s, 요청 : %s \n", userId,action);
+
+        System.out.printf("총 금액 : %,d원\n", 1000000);
+        System.out.printf("주문번호 : ORD-%05d\n", 7666);
+
+
+
+
+//        for (int i = 1; i < 6; i++) {
+//            System.out.printf("=== %d 번 호출===\n", i );
+//            fourMethod(a,b);
+//        }
+
+
 
     }
 
